0d575a5906f3cf0f08f47211cc4dc0de97aa06ab
Guilherme Santos <106110470+guileals@users.noreply.github.com>
2025-04-03 17:49:14 +0000

    First Commit

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,34 @@
-import { useCallback, useEffect, useState } from 'react'
-import Button from '../components/Button'
-import ClickCount from '../components/ClickCount'
-import styles from '../styles/home.module.css'
+import { useCallback, useEffect, useState } from "react";
+import Button from "../components/Button";
+import ClickCount from "../components/ClickCount";
+import styles from "../styles/home.module.css";
 
 function throwError() {
   console.log(
     // The function body() is not defined
     document.body()
-  )
+  );
 }
 
 function Home() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const increment = useCallback(() => {
-    setCount((v) => v + 1)
-  }, [setCount])
+    setCount((v) => v + 1);
+  }, [setCount]);
 
   useEffect(() => {
     const r = setInterval(() => {
-      increment()
-    }, 1000)
+      increment();
+    }, 1000);
 
     return () => {
-      clearInterval(r)
-    }
-  }, [increment])
+      clearInterval(r);
+    };
+  }, [increment]);
 
   return (
     <main className={styles.main}>
-      <h1>Fast Refresh Demo</h1>
+      <h1>First NextJS App</h1>
       <p>
         Fast Refresh is a Next.js feature that gives you instantaneous feedback
         on edits made to your React components, without ever losing component
@@ -56,8 +56,8 @@ function Home() {
         </p>
         <Button
           onClick={(e) => {
-            setTimeout(() => document.parentNode(), 0)
-            throwError()
+            setTimeout(() => document.parentNode(), 0);
+            throwError();
           }}
         >
           Throw an Error
@@ -65,7 +65,7 @@ function Home() {
       </div>
       <hr className={styles.hr} />
     </main>
-  )
+  );
 }
 
-export default Home
+export default Home;
